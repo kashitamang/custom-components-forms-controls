5717ac7aba63d95d94b72ea7a2315f7dd17e150f
KASHI TAMANG <tamang.kashi@gmail.com>
2022-09-28 14:42:12 -0700

    adding TextAreaControl test

--- src/Components/Forms/FormControls.test.js
+++ src/Components/Forms/FormControls.test.js
@@ -1,7 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import {
-  InputControl,
-} from './FormControls.js';
+import { InputControl, TextAreaControl } from './FormControls.js';
 
 test('Input Control', async () => {
   render(
@@ -16,5 +14,22 @@ test('Input Control', async () => {
   const inputControl = screen.getByLabelText('User Name');
   expect(inputControl.name).toBe('username');
   expect(inputControl.placeholder).toBe('Your user name');
+  expect(inputControl.required).toBe(true);
+});
+
+test('Input Control', async () => {
+  render(
+    <TextAreaControl
+      label="Here is your time to shine, tell us your pitch"
+      name="pitch"
+      required
+      placeholder="Shoot for the stars..."
+    />
+  );
+
+  const inputControl = screen
+    .getByLabelText('Here is your time to shine, tell us your pitch');
+  expect(inputControl.name).toBe('pitch');
+  expect(inputControl.placeholder).toBe('Shoot for the stars...');
   expect(inputControl.required).toBe(true);
 });
