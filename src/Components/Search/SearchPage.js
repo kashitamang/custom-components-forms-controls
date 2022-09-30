19e8d7b856860823b4500a17f484f158ce42e81b
KASHI TAMANG <tamang.kashi@gmail.com>
2022-09-30 12:35:39 -0700

    adding use-search hook

--- src/Components/Search/SearchPage.js
+++ src/Components/Search/SearchPage.js
@@ -2,11 +2,14 @@ import styles from './SearchPage.css';
 
 import SearchForm from './SearchForm';
 
-
 export default function SearchPage() {
   return (
     <section>
       <SearchForm/>
+      <ul>
+        Search Results: 
+        <li></li>
+      </ul>
     </section>
   );
 }
