2aad5eb816e3447b7d0bf16ab5b616adc8aaa651
KASHI TAMANG <tamang.kashi@gmail.com>
2022-10-04 10:36:13 -0700

    adding option values to the SelectControl

--- src/Components/Search/SearchForm.js
+++ src/Components/Search/SearchForm.js
@@ -1,11 +1,18 @@
 /* eslint-disable react/prop-types */
-import { InputControl, FormButton } from '../Forms/FormControls.js';
+import { 
+  InputControl, 
+  FormButton,
+SelectControl
+} from '../Forms/FormControls.js';
 import useSearchForm from '../../hooks/use-search-form.js';
+import styles from './Search.css';
 
 export default function SearchForm({ onSubmit }) {
   const { 
     name, 
     setName, 
+    race, 
+    setRace
   } = useSearchForm();
   
   const formSubmit = (e) => {
@@ -16,13 +23,40 @@ export default function SearchForm({ onSubmit }) {
     // console.log('formData', formDataObject);
   };
   return (
-    <form onSubmit={formSubmit}>
+    <form onSubmit={formSubmit} className={styles.SearchForm}>
       <InputControl
         type="text"
+        placeholder="Search By Name "
         name="name"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+      <SelectControl
+        name="race"
+        placeholder="Search By Race"
+        value={race}
+        onChange={(e) => setRace(e.target.value)}
+      >
+        <option value={'Hylian'}>Hylian</option>
+        <option value={'Minish'}>Minish</option>
+        <option value={'Sheikah'}>Sheikah</option>
+        <option value={'Gerudo'}>Gerudo</option>
+        <option value={'Rito'}>Rito</option>
+        <option value={'Mogma'}>Mogma</option>
+        <option value={'Human'}>Human</option>
+        <option value={'Fairy'}>Fairy</option>
+        <option value={'Female Shadow Bug'}>Female Shadow Bug</option>
+        <option value={'Wind Tribe'}>Wind Tribe</option>
+        <option value={'Goron'}>Goron</option>
+        <option value={'Bee'}>Bee</option>
+        <option value={'Zora'}>Zora</option>
+        <option value={'Horse'}>Horse</option>
+        <option value={'Lokomo'}>Lokomo</option>
+        <option value={'Phantom'}>Phantom</option>
+        <option value={'Guardian Deity'}>Guardian Deity</option>
+        <option value={'Korok'}>Korok</option>
+        <option value={'Anouki'}>Anouki</option>
+      </SelectControl>
       <FormButton type="submit">Search</FormButton>
     </form>
   );
